5ec8f19226acc03f7a7a770eaddedc320a7ba376
Maicol Arrollave Alvarez <marrollave@ADADOM.LOCAL>
2025-09-19 10:27:26 -0500

    feat

--- components/CustomTabView.js
+++ components/CustomTabView.js
@@ -9,6 +9,7 @@ export default function CustomTabView({ tabs = [] }) {
   const [activeIndex, setActiveIndex] = useState(0);
   const router = useRouter();
 
+  
   return (
     <div className="items-center justify-center">
       <TabView
